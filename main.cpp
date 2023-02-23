3b03aa590372b2c98146b5bf18085e3745c77893
me <lol@amus.cc>
2023-02-23 10:01:45 -0600

    implemented recursive quicksort.

--- main.cpp
+++ main.cpp
@@ -39,6 +39,28 @@ std::vector<int> bubbleSort(std::vector<int> list) {
     return list;
 }
 
+void quickSortHelper(std::vector<int>& list, int start, int end) {
+    if (start < end) {
+        int pivot = list[end];
+        int i = start - 1;
+        for (int j = start; j < end; j++) {
+            if (list[j] <= pivot) {
+                i++;
+                std::swap(list[i], list[j]);
+            }
+        }
+        std::swap(list[i + 1], list[end]);
+        int pivotIndex = i + 1;
+        quickSortHelper(list, start, pivotIndex - 1);
+        quickSortHelper(list, pivotIndex + 1, end);
+    }
+}
+
+std::vector<int> quickSort(std::vector<int> list) {
+    quickSortHelper(list, 0, list.size() - 1);
+    return list;
+}
+
 int main() {
 
     std::vector<int> vec;
@@ -65,7 +87,7 @@ int main() {
 
     std::cout << "Done!" << std::endl;
 
-    std::cout << "Which algorithm do you want to use?\n 1: Bubble sort" << std::endl;
+    std::cout << "Which algorithm do you want to use?\n 1: Bubble sort\n 2: QuickSort" << std::endl;
     std::cin >> selection;
 
     if (selection == 1){
@@ -74,6 +96,25 @@ int main() {
 
         std::cout << "Sorting... ";
         vec = bubbleSort(vec);
+        std::cout << "Done!" << std::endl;
+
+        std::cout << "Would you like to print out the sorted array? y/n" << std::endl;
+        std::cin >> printask;
+
+        if (printask == 'y'){
+
+            for(int item : vec){
+                std::cout << item << " ";
+            }
+
+        } else {
+            return 0;
+        }
+    } else if (selection == 2){
+        char printask;
+
+        std::cout << "Sorting... ";
+        vec = quickSort(vec);
         std::cout << "Done!" << std::endl;
 
         std::cout << "Would you like to print out the sorted array? y/n" << std::endl;
